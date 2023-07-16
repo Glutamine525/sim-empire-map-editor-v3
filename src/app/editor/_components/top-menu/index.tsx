90ad47e736b1e7057a17946bc9e2b72cd62ab1fc
Glutamine525 <az009931@vip.qq.com>
2023-07-16 18:46:48 +0800

    feat: tooltip shows the version in top menu title

--- src/app/editor/_components/top-menu/index.tsx
+++ src/app/editor/_components/top-menu/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Layout } from '@arco-design/web-react';
+import { Layout, Tooltip } from '@arco-design/web-react';
+import { UI_SETTING, VERSION } from '@/app/editor/_config';
 import TopMenuButton from '../top-menu-button';
 import TopMenuController from '../top-menu-controller';
 import TopMenuCounter from '../top-menu-counter';
 import TopMenuOperation from '../top-menu-operation';
-import { UI_SETTING } from '@/app/editor/_config';
 import styles from './index.module.css';
 
 const Header = Layout.Header;
@@ -17,8 +17,11 @@ const TopMenu = () => {
       className={styles.container}
       style={{
         height: UI_SETTING.topMenuHeight,
-      }}>
-      <div className={styles.title}>模拟帝国布局图编辑器</div>
+      }}
+    >
+      <Tooltip content={`v${VERSION}`}>
+        <div className={styles.title}>模拟帝国布局图编辑器</div>
+      </Tooltip>
       <TopMenuController />
       <TopMenuOperation />
       <TopMenuCounter />
