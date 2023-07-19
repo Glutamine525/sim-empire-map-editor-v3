7b1bfea047e867b794f25813bc997805fc73136d
Glutamine525 <az009931@vip.qq.com>
2023-07-20 00:03:00 +0800

    feat: optimize auto save drawer ui

--- src/app/editor/_components/auto-save-drawer/index.tsx
+++ src/app/editor/_components/auto-save-drawer/index.tsx
@@ -19,6 +19,8 @@ import {
 import { CivilTypeLabel } from '../../_map-core/type';
 import { useAutoSave } from '../../_store/auto-save';
 import { useCommand } from '../../_store/command';
+import { useSetting } from '../../_store/settings';
+import styles from './index.module.css';
 
 interface AutoSaveDrawerProps {
   visible: boolean;
@@ -33,6 +35,7 @@ const AutoSaveDrawer: FC<AutoSaveDrawerProps> = props => {
     state => [state.mapDataStr, state.snapshots, state.moveSelectedToFirst],
     shallow,
   );
+  const limit = useSetting(state => state.autoSaveMaxNum);
 
   const columns: TableColumnProps[] = [
     {
@@ -62,66 +65,78 @@ const AutoSaveDrawer: FC<AutoSaveDrawerProps> = props => {
     {
       title: '操作',
       dataIndex: 'operation',
-      render: (_, { imgSrc, originData }, index) => (
-        <Tooltip
-          position="br"
-          color="var(--color-bg-5)"
-          content={
-            <Image
-              alt="缩略图"
-              src={imgSrc}
-              width={320}
-              height={320}
-              style={{ display: 'block' }}
-            />
-          }
-        >
-          <Button
-            size="mini"
-            type="text"
-            onClick={async () => {
-              if (decodeMapData(mapDataStr)?.md5 === originData.md5) {
-                Message.warning({
-                  id: 'load-auto-save-fail',
-                  content: '载入失败，该存档和当前地图数据一致！',
-                });
-                return;
-              }
-              const isOk = await new Promise<boolean>(resolve => {
-                Modal.confirm({
-                  title: '提示',
-                  content:
-                    '加载完成后，所有历史操作将被清空，是否确认载入该存档？',
-                  onOk: () => {
-                    resolve(true);
-                  },
-                  onCancel: () => {
-                    resolve(false);
-                  },
-                });
-              });
-              if (!isOk) {
-                return;
-              }
-              const { imgSrc: _, ...data } = originData;
-              moveSelectedToFirst(index);
-              importMapData(encodeMapData(data));
-              resetCommand();
-              Message.success('成功载入该存档！');
-              close();
-            }}
+      render: (_, { imgSrc, originData }, index) => {
+        const mapData = decodeMapData(mapDataStr);
+        const isCurrentMap = mapData?.md5 === originData.md5;
+        return (
+          <Tooltip
+            position="br"
+            color="var(--color-bg-5)"
+            content={
+              isCurrentMap ? (
+                '当前地图，禁止载入'
+              ) : (
+                <Image
+                  alt="缩略图"
+                  src={imgSrc}
+                  width={320}
+                  height={320}
+                  style={{ display: 'block' }}
+                />
+              )
+            }
           >
-            载入
-          </Button>
-        </Tooltip>
-      ),
+            <Button
+              size="mini"
+              type="text"
+              disabled={isCurrentMap}
+              style={{ padding: '0 4px' }}
+              onClick={async () => {
+                const isOk = await new Promise<boolean>(resolve => {
+                  Modal.confirm({
+                    title: '提示',
+                    content:
+                      '加载完成后，所有历史操作将被清空，是否确认载入该存档？',
+                    onOk: () => {
+                      resolve(true);
+                    },
+                    onCancel: () => {
+                      resolve(false);
+                    },
+                  });
+                });
+                if (!isOk) {
+                  return;
+                }
+                const { imgSrc: _, ...data } = originData;
+                moveSelectedToFirst(index);
+                importMapData(encodeMapData(data));
+                resetCommand();
+                Message.success('成功载入该存档！');
+                close();
+              }}
+            >
+              载入
+            </Button>
+          </Tooltip>
+        );
+      },
     },
   ];
 
   return (
     <Drawer
       width={640}
-      title={<strong>自动存档</strong>}
+      wrapClassName={styles.container}
+      title={
+        <div className={styles.title}>
+          <strong>自动存档</strong>
+          <div>
+            <strong>{snapshots.length}</strong>
+            <span className={styles.limit}>/{limit}</span>
+          </div>
+        </div>
+      }
       visible={visible}
       focusLock={false}
       onCancel={() => {
@@ -134,7 +149,7 @@ const AutoSaveDrawer: FC<AutoSaveDrawerProps> = props => {
         pagination={false}
         data={snapshots.map((v, i) => ({
           key: i,
-          mapType: v.mapType,
+          mapType: `${v.mapType}木`,
           civil: CivilTypeLabel[v.civil],
           noTree: v.noTree ? '√' : '×',
           buildingNum: Object.values(v.buildings).reduce(
